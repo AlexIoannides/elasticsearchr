3d55092d7af260ad4664baa99e061b51b4e9b7d5
Alex Ioannides <alex.ioannides@yahoo.co.uk>
2016-11-24 13:09:27 +0000

    Minor changes to DESCRIPTION after feedback from CRAN.

--- R/elasticsearchr.R
+++ R/elasticsearchr.R
@@ -1,3 +1,18 @@
+# Copyright 2016 Alex Ioannides
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
 #' elasticsearchr: a lightweight Elasticsearch client for R.
 #'
 #' Allows you to index, update and delete documents as well as run queries and aggregations.
